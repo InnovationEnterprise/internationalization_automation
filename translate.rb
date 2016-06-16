06615859b36395f4d1b7a2734860ba09b4c8d07a
apoirier <alexispoirier@live.com>
2016-06-16 12:59:27 +0100

    Display <% <%= %> tags in three different formats

--- translate.rb
+++ translate.rb
@@ -6,14 +6,21 @@ FILE = ARGV[0]
 class Runner
   def parse
     file_to_translate = File.read(FILE)
-    file_with_erb_tags_escaped = file_to_translate.gsub(/<%([^%]*)%>/, 'BALISE\1BALISE')
+    file_to_translate.gsub!(/<%=/, 'OPEN_DISPLAY_BALISE')
+    file_to_translate.gsub!(/<%/, 'OPEN_BALISE')
+    file_to_translate.gsub!(/%>/, 'CLOSE_BALISE')
+    file_with_erb_tags_escaped = file_to_translate
     doc = Nokogiri::HTML(file_with_erb_tags_escaped)
 
     doc.traverse do |node|
       if node.class == Nokogiri::XML::Text
         string = node.text.strip
 
-        if (string !~ /BALISE/) && string.length > 2
+        #if node.text.match(/\bBALISE\b/)
+        #  node.text.gsub(/\bBALISE\b/, '<%')
+        #end
+
+        if (string !~ /OPEN_DISPLAY_BALISE/ && string !~ /OPEN_BALISE/ && string !~ /CLOSE_BALISE/) && string.length > 2
           puts "Change text: < #{string} > [y/n]"
           answer = $stdin.gets.strip
 
@@ -24,7 +31,6 @@ class Runner
 
           node.content = "replaced!" if answer == 'y'
         end
-
       end
     end
     puts doc
